9d8b552887c0a959791f575b98fdde8c60c034fb
Scheibelr <scheibelr@gmail.com>
2022-01-29 13:00:03 -0500

    Week 2 Workshop completed

---
+++ models/partner.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean
+    }
+}, {
+    timestamps: true
+});
+
+
+const Partner = mongoose.model('Partner', partnerSchema);
+
+module.exports = Partner;
