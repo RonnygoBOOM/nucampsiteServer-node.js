81e97f7c71cffa4ebc59930897b17166665a48e2
Scheibelr <scheibelr@gmail.com>
2022-02-13 13:32:10 -0500

    week 4 complete

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -49,13 +50,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/users', usersRouter);
+app.use('favorites', favoriteRouter);
 
 app.use('/imageUpload', uploadRouter);
 
